e06b2d8cbe96649d053831ac10161be47ae8656a
Manuel Buß <manu@knothorn.dev>
2025-09-27 12:13:45 +0200

    Added Stomach

--- Assets/Scripts/Stomach.cs
+++ Assets/Scripts/Stomach.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
@@ -10,12 +11,26 @@ public class Stomach : MonoBehaviour
     [SerializeField] private Transform[] stationPointsBrain;
     [SerializeField] private Transform[] stationPointsHeart;
     [SerializeField] private Transform[] stationPointsStomach;
+    
+    [SerializeField] private int maxFactChecker;
+    
+    
+    [SerializeField] private int minFood;
+    [SerializeField] private int maxFood;
+    [SerializeField] private int minFoodSpawnInterval;
+    [SerializeField] private int maxFoodSpawnInterval;
+    [SerializeField] private GameObject[] foodPrefabs;
+    [SerializeField] private Transform[] foodSpawns;
 
     private List<Transform> availablePointsBrain;
     private List<Transform> availablePointsHeart;
     private List<Transform> availablePointsStomach;
+    private List<Transform> availableFoodSpawns;
 
     private List<Spreader> stations;
+
+    private int currentFactChecker;
+    private int foodSpawnCounter;
 
 
     private void Awake()
@@ -23,12 +38,48 @@ public class Stomach : MonoBehaviour
         availablePointsBrain = new List<Transform>(stationPointsBrain);
         availablePointsHeart = new List<Transform>(stationPointsHeart);
         availablePointsStomach = new List<Transform>(stationPointsStomach);
+        availableFoodSpawns = new List<Transform>(foodSpawns);
         stations = new List<Spreader>();
     }
 
+    private void Start()
+    {
+        GameManager.Instance.OnTick += Tick;
+    }
+
+    private void Tick()
+    {
+        foodSpawnCounter--;
+        if (foodSpawnCounter <= 0)
+        {
+            foodSpawnCounter = Random.Range(minFoodSpawnInterval, maxFoodSpawnInterval+1);
+            StartCoroutine(SpawnFood());
+        }
+    }
+
+    public void BuyFactChecker()
+    {
+        currentFactChecker++;
+    }
+
+    public void CreateStomachStation()
+    {
+        CreatStation(ref availablePointsStomach);
+    }
+    
+    public void CreateHeartStation()
+    {
+        CreatStation(ref availablePointsHeart);
+    }
+
+    public void CreateBrainStation()
+    {
+        CreatStation(ref availablePointsBrain);
+    }
 
     private void CreatStation(ref List<Transform> points)
     {
+        if(points.Count <= 0) return;
         int index = Random.Range(0, points.Count);
         Vector3 position = points[index].position;
         points.RemoveAt(index);
@@ -37,5 +88,37 @@ public class Stomach : MonoBehaviour
         stations.Add(spreader);
     }
 
+    private IEnumerator SpawnFood()
+    {
+        int foodAmount = Random.Range(minFood, maxFood + 1);
+        availableFoodSpawns = new List<Transform>(foodSpawns);
+        float totalCorruption = 0;
+        for (int i = 0; i < foodAmount; i++)
+        {
+            int index = Random.Range(0, availableFoodSpawns.Count);
+            Instantiate(foodPrefabs[Random.Range(0,foodPrefabs.Length)], availableFoodSpawns[index].position, Quaternion.identity);
+            availableFoodSpawns.RemoveAt(index);
 
+            totalCorruption += Random.Range(-1f, 1f);
+            
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        totalCorruption /= foodAmount;
+
+        if (totalCorruption > 0)
+        {
+            totalCorruption *= 1 - (currentFactChecker / (float)maxFactChecker);
+        }
+        
+        UpdateStations(totalCorruption);
+    }
+
+    private void UpdateStations(float value)
+    {
+        foreach (Spreader spreader in stations)
+        {
+            spreader.Value = value;
+        }
+    }
 }
