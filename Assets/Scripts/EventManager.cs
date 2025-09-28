0de464ed304ccf736d9468154c2760e9e839f04e
Simon Hetzer (MacBook) <simon.hetzer@icloud.com>
2025-09-28 10:07:49 +0200

    Event UI

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -10,13 +10,20 @@ public class EventManager : MonoBehaviour
     [SerializeField] private float strongVirusAttack = 0.4f;
     [SerializeField] private int durationActiveVirusAttack = 10;
     private const string IDVirusAttack = "Virus";
+    [SerializeField] private string virusDescription;
+    [SerializeField] private Sprite virusIcon;
 
     [Header("Freeze"), SerializeField] private int brainFreezeDuration = 5;
     private const string IDFreeze = "Freeze";
+    [SerializeField] private string brainFreezeDescription;
+    [SerializeField] private Sprite brainFreezeIcon;
 
     [Header("Brain Corruption"),SerializeField] private float weakBrainCorruption = 0.2f;
     [SerializeField] private float mediumBrainCorruption = 0.4f;
     [SerializeField] private float strongBrainCorruption = 0.5f;
+    [SerializeField] private string brainFogDescription;
+    [SerializeField] private Sprite brainFogIcon;
+    [SerializeField] private float brainFogInfoDuration = 3.0f;
     
     
 
@@ -28,10 +35,13 @@ public class EventManager : MonoBehaviour
     private string currentEvent = "";
     private int currentEventDuration;
     private int ticksTillNext;
+    
+    private EventUI eventUI;
 
     private void Start()
     {
         GameManager.Instance.OnEarlyTick += Tick;
+        eventUI = FindFirstObjectByType<EventUI>();
     }
 
     private void Tick()
@@ -90,6 +100,7 @@ public class EventManager : MonoBehaviour
         }
         
         Debug.Log("Triggered: Brain Corruption" );
+        eventUI.Show("Brain Fog!", brainFogDescription, brainFogIcon, brainFogInfoDuration);
     }
 
 
@@ -99,6 +110,7 @@ public class EventManager : MonoBehaviour
         currentEvent = IDFreeze;
         currentEventDuration = brainFreezeDuration;
         Debug.Log("Triggered: " + IDFreeze);
+        eventUI.Show("Brain Freeze!", brainFreezeDescription, brainFreezeIcon);
     }
     
     private void TriggerVirusAttack()
@@ -119,6 +131,7 @@ public class EventManager : MonoBehaviour
         currentEvent = IDVirusAttack;
         currentEventDuration = durationActiveVirusAttack;
         Debug.Log("Triggered: " + IDVirusAttack);
+        eventUI.Show("Virus Attack!", virusDescription, virusIcon);
     }
 
     private void StopCurrentEvent()
@@ -137,5 +150,6 @@ public class EventManager : MonoBehaviour
 
         currentEvent = "";
         currentEventDuration = 0;
+        eventUI.Hide();
     }
 }
