fbb239a47d21547abfeac0d910550ab33dcbd3d0
Simon Hetzer (MacBook) <simon.hetzer@icloud.com>
2025-09-28 13:26:39 +0200

    Fixed MainMenu Anchors

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,8 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject audio;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(audio);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("BeanTesting");
